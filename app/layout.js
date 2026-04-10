615a475de7bae35eb0ef47f99bd96fa1732bfef1
Susovan <susovan.sahoo777@gmail.com>
2026-04-10 10:46:06 +0530

    Added Metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 export const metadata = {
-  title: "Wild Oasis | Travel Made Easy",
-  description: "Next App",
+  // title: "Wild Oasis | Travel Made Easy",
+  title: {
+    template: '%s | Wild Oasis',
+    default: 'Welcome | Wild Oasis',
+  },
+  description:
+    'Luxarious cabin hotel, surrounded by beautiful mountains and dark forest',
+  icons: {
+    icon: '/icon.png', // custom location
+  },
 };
 
 export default function RootLayout({ children }) {
